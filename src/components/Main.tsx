bbf023e4814043a5634366ef261ee85ed5494824
Andrew Kaye <notandrewkaye@gmail.com>
2021-04-28 16:29:47 -0700

    main card layout

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,24 +1,36 @@
 
 const Main = () => {
   return (
-    <section className="sans-serif cf pa3 pa4-m pa5-l mw9 center">
-      <article className="cf">
-        <div className="fl w-100 w-50-ns bg-near-white tc">
-          <h1>Our Team</h1>
+    <>
+    <section className="sans-serif white cf ph3 ph4-m ph5-l pt3 pt4-m pt5-l mw9 center">
+      <article className="flex flex-wrap">
+        <div className="fl w-100 w-50-l tc">
+          <a href="#" className="link dim db mr4-l bg-fp-blue pv6 white">
+            <h3 className="mv0">Our Team</h3>
+          </a>
         </div>
-        <div className="fl w-100 w-50-ns bg-light-gray tc">
-          <h1>Our Philosophy</h1>
+        <div className="fl w-100 w-50-l tc">
+          <a href="#" className="link dim db ml4-l mt4 mt0-l bg-fp-yellow pv6 white">
+            <h3 className="mv0">Our Philosophy</h3>
+          </a>
         </div>
       </article>
-      <article className="cf">
-        <div className="fl w-100 w-50-ns bg-near-white tc">
-          <h1>Our Work</h1>
+    </section>    
+    <section className="sans-serif white cf pa3 pa4-m pa5-l mw9 center">
+      <article className="flex flex-wrap">
+        <div className="fl w-100 w-50-l tc">
+          <a href="medium.com/flexport-design" className="link dim db mr4-l bg-fp-red pv6 white">
+            <h3 className="mv0">Our Work</h3>
+          </a>
         </div>
-        <div className="fl w-100 w-50-ns bg-light-gray tc">
-          <h1>Our Design System</h1>
+        <div className="fl w-100 w-50-l tc">
+          <a href="flexport.com/design" className="link dim db ml4-l mt4 mt0-l bg-fp-purple pv6 white">
+            <h3 className="mv0">Our Design System</h3>
+          </a>
         </div>
       </article>
     </section>
+    </>
   )};
 
   export default Main;
